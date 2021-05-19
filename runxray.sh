d82bd019a8aeb6039ae8486427ca2de0b786b277
iamtrazy <iamtrazy@protonmail.com>
2021-05-19 15:07:23 +0530

    Update runxray.sh

--- runxray.sh
+++ runxray.sh
@@ -1,3 +1,32 @@
 #!/bin/sh
+
+# config xray
+cat << EOF > /etc/xray/config.json
+{
+  "inbounds":[
+    {
+      "port": $PORT,
+      "protocol": "$PROTOCOL",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}	
+EOF
+
 # run xray
 /usr/bin/xray run -config /etc/xray/config.json

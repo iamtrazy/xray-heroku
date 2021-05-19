2b188af500547b5ee4400d09713527d0f8f45c9a
iamtrazy <iamtrazy@protonmail.com>
2021-05-19 16:14:23 +0530

    Update runxray.sh

--- runxray.sh
+++ runxray.sh
@@ -2,65 +2,23 @@
 # config xray
 cat << EOF > /etc/xray/config.json
 {
-    "inbounds": [
-	{
-	"port": 80,
-	"protocol": "$PROTOCOL",
-	"settings": {
-		"clients": [
-					{
-						"id": "$UUID",
-						"level": 1,
-						"alterId": 4,
-						"security": "auto"
-					}
-		]
-	},
-	"streamSettings": {
-		"network": "tcp",
-		"tcpSettings": {
-			"header": {
-				"type": "http",
-				"response": {
-					"version": "1.1",
-					"status": "200",
-					"reason": "OK",
-					"headers": {
-						"Content-encoding": [
-							"gzip"
-						],
-						"Content-Type": [
-							"text/html; charset=utf-8"
-						],
-						"Cache-Control": [
-							"no-cache"
-						],
-						"Vary": [
-							"Accept-Encoding"
-						],
-						"X-Frame-Options": [
-							"deny"
-						],
-						"X-XSS-Protection": [
-							"1; mode=block"
-						],
-						"X-content-type-options": [
-							"nosniff"
-						]
-					}
-				}
-			}
-		}
-	},
-	"sniffing": {
-		"enabled": true,
-		"destOverride": [
-			"http",
-			"tls"
-		]
-	}
-}
-],
+  "inbounds":[
+    {
+      "port": $PORT,
+      "protocol": "$PROTOCOL",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "$UUID"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
   "outbounds": [
     {
       "protocol": "freedom"

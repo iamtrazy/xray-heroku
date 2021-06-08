290099045b138fe899dd5914971ade633115fd8f
iamtrazy <iamtrazy@protonmail.com>
2021-06-08 18:43:55 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,6 @@
 setup-timezone -z Asia/Colombo
 apk del alpine-conf
 
-#enable bbr
-echo net.core.default_qdisc=fq >> /etc/sysctl.conf
-echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
-sysctl -p
-
 #Config xray
 rm -rf /etc/xray/config.json
 cat << EOF > /etc/xray/config.json
